f92a30447f90a1ce08357d87c98578c387ff9c65
Sandeep Kumar <sandeep.k@daffodilsw.com>
2023-07-09 21:00:37 +0530

    First commit

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import * as bootstrap from "bootstrap"
+import ApexCharts from 'apexcharts';
+window.ApexCharts = ApexCharts; // return apex chart 
+import 'tabler/tabler'
+import 'tabler/demo'
